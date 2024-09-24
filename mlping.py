95ef2220d6807e63fb6d6afe3f4988cb713f7bb1
Sheshasai <trashdude177@gmail.com>
2024-09-24 19:39:54 +0530

    debuggin to know on which website the script is having trouble

--- mlping.py
+++ mlping.py
@@ -93,6 +93,7 @@ sites = [
 ]
 
 url = random.choice(sites)
+print(url)
 
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
